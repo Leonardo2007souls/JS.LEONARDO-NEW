c285224b818bbf825502f1b56d2e454bd85f44d4
LEONARDO <leonardo.dias.santos@escola.pr.gov.br>
2024-08-14 13:59:13 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 500);
+  background("green");//cor do fundo 
+}
+
+function draw() {
+  
+  
+  stroke("red");
+  
+  fill("blue");
+  
+  if(mouseIsPressed)
+    
+(rect(mouseX,mouseY,80,80))// cria um retangulo
+  
+  
+  
+}
